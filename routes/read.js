5fd531ac0e3714a21af40205f1693861509dc3be
devanshdalmia01 <devanshdalmia1@gmail.com>
2022-11-28 21:00:58 +0530

    bug fix

--- routes/read.js
+++ routes/read.js
@@ -4,7 +4,7 @@ import { readAllHazards, readAllHazardsOfUser } from "../controllers/read.js";
 
 const router = express.Router();
 
-router.get("/allhazards", authenticate, readAllHazards);
+router.get("/allhazards", readAllHazards);
 router.get("/allhazardsofuser", authenticate, readAllHazardsOfUser);
 
 export default router;
