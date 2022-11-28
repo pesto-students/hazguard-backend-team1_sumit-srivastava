4fea24e8e2b9bc8ebc8b5528b4bde0c67cf3c850
devanshdalmia01 <devanshdalmia1@gmail.com>
2022-11-29 04:08:20 +0530

    bug fix

--- controllers/read.js
+++ controllers/read.js
@@ -1,7 +1,7 @@
 import Hazard from "../models/hazard.js";
 
 const readAllHazards = (req, res) => {
-	Hazard.find({ isPublic: true }, function (err, doc) {
+	Hazard.find(function (err, doc) {
 		if (err) return res.status(500).json({ error: true, message: err });
 		if (!doc.length) return res.status(404).json({ error: true, message: "No hazards available!" });
 		return res.status(200).json({
@@ -10,14 +10,4 @@ const readAllHazards = (req, res) => {
 	});
 };
 
-const readAllHazardsOfUser = (req, res) => {
-	Hazard.find({ userId: req.user.userId }, function (err, doc) {
-		if (err) return res.status(500).json({ error: true, message: err });
-		if (!doc.length) return res.status(404).json({ error: true, message: "No hazards available for user!" });
-		return res.status(200).json({
-			data: doc,
-		});
-	});
-};
-
-export { readAllHazards, readAllHazardsOfUser };
+export { readAllHazards };
--- routes/read.js
+++ routes/read.js
@@ -1,10 +1,8 @@
 import express from "express";
-import { authenticate } from "../controllers/auth.js";
-import { readAllHazards, readAllHazardsOfUser } from "../controllers/read.js";
+import { readAllHazards } from "../controllers/read.js";
 
 const router = express.Router();
 
 router.get("/allhazards", readAllHazards);
-router.get("/allhazardsofuser", authenticate, readAllHazardsOfUser);
 
 export default router;
