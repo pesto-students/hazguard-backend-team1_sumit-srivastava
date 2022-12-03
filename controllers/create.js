34000b4a0f8ba03775b724465cc836d62db08620
devanshdalmia01 <devanshdalmia1@gmail.com>
2022-12-03 16:29:08 +0530

    adding hazard images

--- controllers/create.js
+++ controllers/create.js
@@ -1,7 +1,6 @@
 import Hazard from "../models/hazard.js";
 import User from "../models/user.js";
 import { sendEmail } from "../utils/mail.js";
-import { v4 } from "uuid";
 
 const createHazard = (req, res) => {
 	User.findOne({ userId: req.user.userId }, function (err, user) {
@@ -9,7 +8,7 @@ const createHazard = (req, res) => {
 		if (!user) return res.status(404).json({ error: true, message: "User not found!" });
 		const newHazard = new Hazard({
 			userId: req.user.userId,
-			hazardId: v4(),
+			hazardId: req.body.hazardId,
 			type: req.body.type,
 			industry: req.body.industry,
 			department: req.body.department,
