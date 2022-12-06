611b65fd51aac61543ed84a629a7dfe45b968ceb
devanshdalmia01 <devanshdalmia1@gmail.com>
2022-12-06 18:42:22 +0530

    bug fix

--- controllers/read.js
+++ controllers/read.js
@@ -4,7 +4,7 @@ import Hazard from "../models/hazard.js";
 const readAllHazards = (req, res) => {
 	Hazard.find(function (err, doc) {
 		if (err) return res.status(500).json({ error: true, message: err });
-		if (!doc.length) return res.status(404).json({ error: true, message: "No hazards available!" });
+		if (!doc.length) return res.status(200).json({ error: true, message: "No hazards available!" });
 		return res.status(200).json({
 			data: doc,
 		});
